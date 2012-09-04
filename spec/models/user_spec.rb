659240f9f6435b0fa1f4ddc496a6714da2d9366b
ueli kunz <kunz@ideadapt.net>
2012-09-04 17:44:01 +0200

    user model with secure password

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,80 @@
+require 'spec_helper'
+
+describe User do
+
+  before { @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar") }
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+
+  describe "when password is not present" do
+    before { @user.password = @user.password_confirmation = " " }
+    it { should_not be_valid }
+  end
+
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should_not be_valid }
+  end
+
+  describe "return value of authenticate method" do
+    before { @user.save }
+    let(:found_user) { User.find_by_email(@user.email) }
+
+    describe "with valid password" do
+      it { should == found_user.authenticate(@user.password) }
+    end
+
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+      it { should_not == user_for_invalid_password }
+      specify { user_for_invalid_password.should be_false } # specify is synonym for it
+    end
+  end
+
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
+
+  describe "when password confirmation is nil" do
+    before { @user.password_confirmation = nil }
+    it { should_not be_valid }
+  end
+
+  describe "email is invalid" do
+
+    it "all should be invalid" do
+      ["ueli@hsr.ch.+", "@mine@hsr.ch", ",!?ueli@hsr.ch"].each do |addr|
+        @user.email = addr
+        @user.should_not be_valid
+      end
+    end
+  end
+
+  describe "email is valid" do
+
+    it "all should be valid" do
+      ["ueli@hsr.ch.", "mine@hsr.ch", "some.ueli@hsr.ch"].each do |addr|
+        @user.email = addr
+        @user.should be_valid
+      end
+    end
+  end
+
+  describe "when email address is already taken" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+    end
+
+    it { should_not be_valid }
+  end
+end
