56b5c8ee3b95b9c72f61ba89bce00dc27d7ba4f5
ueli kunz <kunz@ideadapt.net>
2012-09-04 15:48:04 +0200

    layout enhanced

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
+  root to: "static_pages#home"
+  match "/home", to: "static_pages#home"
+  match "/static_pages", to: "static_pages#home"
 
-  get "static_pages/help"
+  match "/help", to: "static_pages#help"
+  match "/about", to: "static_pages#about"
 
-  get "static_pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
